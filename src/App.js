7813695a40f684e416cdfce28fe0b8227e4db85b
missionreadyhamish <hamishe@missionreadyhq.com>
2025-03-18 15:51:39 +1300

    feat: chatbox function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./styles/App.css";
 import Header from "./components/Header";
+import ChatBox from "./components/Chatbox";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <ChatBox />
     </div>
   );
 }
