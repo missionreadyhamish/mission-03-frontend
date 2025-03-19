ca999d718ff5e8e348c3b5af1df9b19b4ef6768b
Tom Atkinson @cheese <tom@funk.co.nz>
2025-03-19 23:53:11 +1300

    deletes Hamish's .env and adds it to .gitignore in next commit

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const axios = require("axios");
 require("dotenv").config();
 
 const app = express();
-const port = 5000;
+const port = 5001;
 
 // Initialize Google Gemini AI with API key from .env file
 const apiKey = process.env.GOOGLE_API_KEY;
