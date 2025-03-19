54574b1110454646977848dc6624920ab21e87bb
Tom Atkinson @cheese <tom@funk.co.nz>
2025-03-20 09:05:28 +1300

    nice looking footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,36 +6,31 @@ import { GiGiftOfKnowledge, GiHealthNormal } from "react-icons/gi";
 const Header = () => {
   return (
     <footer className={styles.footer}>
-            <div className={styles.griditem}>
-                <div className={styles.left}>
-                    <button className={styles.button}>
-                        <MdDiversity3 className={styles.icon} />
-                        Diversity &amp; Inclusion
-                    </button>
-                </div>
+        <div className={styles.griditem}>
+            <div className={styles.left}>
+                <a href="https://turnerspeople.co.nz/turners-cars/turners-cars-diversity-and-inclusion/"><button href="https://turnerspeople.co.nz/turners-cars/turners-cars-diversity-and-inclusion/" className={styles.button}>
+                    <MdDiversity3 className={styles.icon} />
+                    Diversity &amp; Inclusion
+                </button></a>
             </div>
-
-            <div className={styles.griditem}>
-                <div className={styles.middle}>
-                    <button className={styles.button}>
-                        <GiGiftOfKnowledge className={styles.icon} />
-                        Learning &amp; Development
-                    </button>
-                </div>
+        </div>
+        <div className={styles.griditem}>
+            <div className={styles.middle}>
+                <a href="https://turnerspeople.co.nz/turners-cars/turners-cars-learning-development/"><button className={styles.button}>
+                    <GiGiftOfKnowledge className={styles.icon} />
+                    Learning &amp; Development
+                </button></a>
             </div>
-            <div className={styles.griditem}>
-                <div className={styles.right}>
+        </div>
+        <div className={styles.griditem}>
+            <div className={styles.right}>
+                    <a href="https://turnerspeople.co.nz/turners-cars/turners-cars-health-safety-wellbeing/">
                     <button className={styles.button}>
-                        <GiHealthNormal className={styles.icon} />
-                        Health, Safety &amp; Wellbeing
-                    </button>
-                </div>
+                    <GiHealthNormal className={styles.icon} />
+                    Health, Safety &amp; Wellbeing
+                </button></a>
             </div>
-
-
-
-
-
+        </div>
     </footer>
   );
 };
